b515a7d567a65ceb2a2e503f359f7acc4b7a4b59
MhmCats <71032999+MhmCats@users.noreply.github.com>
2021-03-05 11:50:20 +0000

    json file startup logic

--- src/main.py
+++ src/main.py
@@ -9,6 +9,17 @@ if __name__ == "__main__":
     print("Avoid!\n\nA game where you must avoid all obsticales in your path and be as fast as possible."
           "\n\nControls\n\nThe controls are WASD\n\nMisc\n\nThe arrow in the corner of the screen in the game"
           "is a back button that takes you to the menu.\n\nFind the source code at https://github.com/MhmCats/Avoid.\n\n")
+    
+    if not os.path.exists("resources/highscores.json"):
+        with open("resources/highscores.json", "w") as f:
+            json_file_content = """\
+{
+    "level-one": 0,
+    "level-two": 0,
+    "level-three": 0
+}"""
+            f.write(json_file_content)
+
     try:
         start_menu = menu.Menu() # The Menu class' __init__ being called
         start_menu.start()       # and then the start method.
