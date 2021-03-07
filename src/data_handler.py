fb05871b7a1edfe0acb0e26b4abef4fb2f20433c
MhmCats <71032999+MhmCats@users.noreply.github.com>
2021-03-07 12:59:39 +0000

    Changed levels/ to level_info/ and moved highscores.json there

--- src/data_handler.py
+++ src/data_handler.py
@@ -5,12 +5,12 @@ import copy
 # Nothing really special....
 
 def load_level(level_id: str):
-    with open(f"levels/{level_id}.json") as json_file:
+    with open(f"level_info/{level_id}.json") as json_file:
         res = json.load(json_file)
         return res[level_id]
 
 def get_highscores(level_id: str):
-	with open("resources/highscores.json") as json_file:
+	with open("level_info/highscores.json") as json_file:
 		res = json.load(json_file)
 		return res[level_id]
 
@@ -33,5 +33,5 @@ def update_highscores(level_id: str, new_highscore: int):
 
 	new_content += "\n}"
 
-	with open("resources/highscores.json", "w") as json_file:
+	with open("level_info/highscores.json", "w") as json_file:
 		json_file.write(new_content)
--- src/main.py
+++ src/main.py
@@ -6,12 +6,9 @@ import menu
 # Importing the menu as we have to start off in the menu screen
 
 if __name__ == "__main__":
-    print("Avoid!\n\nA game where you must avoid all obsticales in your path and be as fast as possible."
-          "\n\nControls\n\nThe controls are WASD\n\nMisc\n\nThe arrow in the corner of the screen in the game"
-          "is a back button that takes you to the menu.\n\nFind the source code at https://github.com/MhmCats/Avoid.\n\n")
     
-    if not os.path.exists("resources/highscores.json"):
-        with open("resources/highscores.json", "w") as f:
+    if not os.path.exists("level_info/highscores.json"):
+        with open("level_info/highscores.json", "w") as f:
             json_file_content = """\
 {
     "level-one": 0,
