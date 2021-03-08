67b4d343e3cb9e71d1f5f513a8bf0a1582c588d0
MhmCats <71032999+MhmCats@users.noreply.github.com>
2021-03-08 10:40:08 +0000

    Made the start sound optional :(

--- src/main.py
+++ src/main.py
@@ -2,13 +2,22 @@ import turtle
 import os
 import _tkinter
 
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--startsound",
+                    help="Play the start sound or not.", 
+                    action="store_true")
+
+args = parser.parse_args()
+
 import menu
 # Importing the menu as we have to start off in the menu screen
 
 if __name__ == "__main__":
 
     try:
-        start_menu = menu.Menu(play=True) # The Menu class' __init__ being called
+        start_menu = menu.Menu(play=args.startsound) # The Menu class' __init__ being called
         start_menu.start()                # and then the start method.
     except KeyboardInterrupt:             # This was just for a smooth exit.
         print("\nExiting...")
